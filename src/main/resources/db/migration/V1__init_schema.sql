d08074a90dff707867f260abd77a26f3432c4b83
ngurahbas <ngurahbaskara@gmail.com>
2025-12-19 21:04:49 +0800

    add initial data migration

---
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,24 @@
+create or replace function update_updated_at_column()
+    returns trigger as
+$$
+begin
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    return NEW;
+end;
+$$ language plpgsql;
+
+create table identifier
+(
+    id         bigserial primary key,
+    type       varchar(10) check ( type in ('email', 'mobile')) not null,
+    value      varchar(255)                                     not null,
+    created_at TIMESTAMPTZ                                      NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ                                      NOT NULL DEFAULT now()
+);
+
+drop trigger if exists set_updated_at_identifier on identifier;
+create trigger set_updated_at_identifier
+    before update
+    on identifier
+    for each row
+execute procedure update_updated_at_column();
