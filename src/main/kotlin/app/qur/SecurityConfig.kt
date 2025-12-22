160442f73eab66d1545bc7ec0a2fba250c188ff9
ngurahbas <ngurahbaskara@gmail.com>
2025-12-22 09:57:12 +0800

    reformat abit

--- src/main/kotlin/app/qur/SecurityConfig.kt
+++ src/main/kotlin/app/qur/SecurityConfig.kt
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers
 
 @Configuration
 class SecurityConfig {
@@ -17,8 +16,7 @@ class SecurityConfig {
                     .anyExchange().authenticated()
             }
             .oauth2Login { oauth2 ->
-                oauth2
-                    .loginPage("/login")
+                oauth2.loginPage("/login")
             }
             .oauth2Client { }
             .csrf { csrf -> csrf.disable() }
