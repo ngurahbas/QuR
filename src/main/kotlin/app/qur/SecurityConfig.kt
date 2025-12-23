c9e13b31cc7c6afba599bd0e28331eb50cf21b39
ngurahbas <ngurahbaskara@gmail.com>
2025-12-23 23:47:44 +0800

    trim data

--- src/main/kotlin/app/qur/SecurityConfig.kt
+++ src/main/kotlin/app/qur/SecurityConfig.kt
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthorizationCodeAuthenticationToken
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationCodeAuthenticationTokenConverter
+import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler
 import org.springframework.web.server.ServerWebExchange
@@ -30,7 +32,33 @@ class SecurityConfig(
                     .authenticationConverter(object :
                         ServerOAuth2AuthorizationCodeAuthenticationTokenConverter(reactiveClientRegistrationRepository) {
                         override fun convert(exchange: ServerWebExchange): Mono<Authentication> {
-                            return super.convert(exchange)
+                            return super.convert(exchange).map { authentication ->
+                                val token = authentication as OAuth2AuthorizationCodeAuthenticationToken
+                                val oauth2User = token.principal as? OAuth2User
+                                
+                                if (oauth2User == null) {
+                                    authentication
+                                } else {
+                                    val email = oauth2User.getAttribute<String>("email") 
+                                        ?: throw IllegalArgumentException("Email not found")
+                                    
+                                    val clientRegistration = token.clientRegistration
+                                    val authorizationExchange = token.authorizationExchange
+                                    val accessToken = token.accessToken
+                                    val refreshToken = token.refreshToken
+                                    val additionalParameters = token.additionalParameters
+                                    
+                                    object : OAuth2AuthorizationCodeAuthenticationToken(
+                                        clientRegistration,
+                                        authorizationExchange,
+                                        accessToken,
+                                        refreshToken,
+                                        additionalParameters
+                                    ) {
+                                        override fun getPrincipal(): Any = EmailPrincipal(email)
+                                    }
+                                }
+                            }
                         }
                     })
             }
@@ -39,3 +67,5 @@ class SecurityConfig(
             .build()
     }
 }
+
+class EmailPrincipal(val email: String)
