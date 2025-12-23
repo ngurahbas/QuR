be0c0c775a6a233ef1f277cf34ec914660f3df93
ngurahbas <ngurahbaskara@gmail.com>
2025-12-23 20:45:13 +0800

    preparing for AI

--- src/main/kotlin/app/qur/SecurityConfig.kt
+++ src/main/kotlin/app/qur/SecurityConfig.kt
@@ -3,11 +3,18 @@ package app.qur
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationCodeAuthenticationTokenConverter
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val reactiveClientRegistrationRepository: ReactiveClientRegistrationRepository
+) {
 
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
@@ -20,6 +27,12 @@ class SecurityConfig {
             .oauth2Login { oauth2 ->
                 oauth2.loginPage("/login")
                     .authenticationSuccessHandler(RedirectServerAuthenticationSuccessHandler("/hello"))
+                    .authenticationConverter(object :
+                        ServerOAuth2AuthorizationCodeAuthenticationTokenConverter(reactiveClientRegistrationRepository) {
+                        override fun convert(exchange: ServerWebExchange): Mono<Authentication> {
+                            return super.convert(exchange)
+                        }
+                    })
             }
             .oauth2Client { }
             .csrf { csrf -> csrf.disable() }
