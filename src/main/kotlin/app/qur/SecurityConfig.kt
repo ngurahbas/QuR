06788a5b49d4f26a71797eaf3434401bd61624d7
ngurahbas <ngurahbaskara@gmail.com>
2025-12-27 20:08:37 +0800

    reduce security config to bare minimum

--- src/main/kotlin/app/qur/SecurityConfig.kt
+++ src/main/kotlin/app/qur/SecurityConfig.kt
@@ -3,16 +3,9 @@ package app.qur
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthorizationCodeAuthenticationToken
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationCodeAuthenticationTokenConverter
-import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler
-import org.springframework.web.server.ServerWebExchange
-import reactor.core.publisher.Mono
 
 @Configuration
 class SecurityConfig(
@@ -28,45 +21,7 @@ class SecurityConfig(
                     .anyExchange().authenticated()
             }
             .oauth2Login { oauth2 ->
-                oauth2.loginPage("/login") //TODO: somehow spring boot standard /login inconflict with app.qur.web.AuthController.login
-                    .authenticationSuccessHandler(RedirectServerAuthenticationSuccessHandler("/hello"))
-                    .authenticationConverter(object :
-                        ServerOAuth2AuthorizationCodeAuthenticationTokenConverter(reactiveClientRegistrationRepository) {
-                        override fun convert(exchange: ServerWebExchange): Mono<Authentication> {
-                            return super.convert(exchange).map { authentication ->
-                                val token = authentication as OAuth2AuthorizationCodeAuthenticationToken
-                                val oauth2User = token.principal as? OAuth2User
-                                
-                                if (oauth2User == null) {
-                                    authentication
-                                } else {
-                                    val email = oauth2User.getAttribute<String>("email") 
-                                        ?: throw IllegalArgumentException("Email not found")
-
-                                    object : Authentication {
-                                        var _authenticated = true
-
-                                        override fun isAuthenticated() = _authenticated
-
-                                        override fun setAuthenticated(isAuthenticated: Boolean) {
-                                            _authenticated = isAuthenticated
-                                        }
-
-                                        override fun getName() = null
-
-                                        override fun getPrincipal() = EmailPrincipal(email)
-
-                                        override fun getAuthorities() = emptyList<GrantedAuthority>()
-
-                                        override fun getCredentials() = null
-
-                                        override fun getDetails() = null
-
-                                    }
-                                }
-                            }
-                        }
-                    })
+                oauth2.authenticationSuccessHandler(RedirectServerAuthenticationSuccessHandler("/hello"))
             }
             .oauth2Client { }
             .csrf { csrf -> csrf.disable() }
