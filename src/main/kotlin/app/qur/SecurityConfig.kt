56bf4327944f6ff860f59227a70eefc50b531bb9
ngurahbas <ngurahbaskara@gmail.com>
2025-12-28 17:16:42 +0800

    remove unused code. prepare principal class

--- src/main/kotlin/app/qur/SecurityConfig.kt
+++ src/main/kotlin/app/qur/SecurityConfig.kt
@@ -3,14 +3,11 @@ package app.qur
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler
 
 @Configuration
-class SecurityConfig(
-    private val reactiveClientRegistrationRepository: ReactiveClientRegistrationRepository
-) {
+class SecurityConfig {
 
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
@@ -28,5 +25,3 @@ class SecurityConfig(
             .build()
     }
 }
-
-class EmailPrincipal(val email: String)
---
+++ src/main/kotlin/app/qur/service/CustomOidcUser.kt
@@ -0,0 +1,18 @@
+package app.qur.service
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
+
+class CustomOidcUser(val email: String): OidcUser {
+    override fun getClaims() = mapOf<String, Any>()
+
+    override fun getUserInfo() = null
+
+    override fun getIdToken() = null
+
+    override fun getAttributes() = mapOf<String, Any>()
+
+    override fun getAuthorities() = emptyList<GrantedAuthority>()
+
+    override fun getName() = email
+}
