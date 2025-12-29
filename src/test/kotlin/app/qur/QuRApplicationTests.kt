1250de7bab7a512705c695f7c25a8ce60d103736
ngurahbas <ngurahbaskara@gmail.com>
2025-12-29 15:48:32 +0800

    using pgsql test container

--- src/test/kotlin/app/qur/QuRApplicationTests.kt
+++ src/test/kotlin/app/qur/QuRApplicationTests.kt
@@ -1,9 +1,14 @@
 package app.qur
 
+import app.qur.db.TestContainersConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
+@Import(TestContainersConfig::class)
+@Testcontainers
 class QuRApplicationTests {
 
 	@Test
