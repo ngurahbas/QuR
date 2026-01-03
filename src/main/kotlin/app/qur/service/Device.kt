b3a6653ea91d437c6fc709c2222cca7ee0bef55c
ngurahbas <ngurahbaskara@gmail.com>
2026-01-03 13:32:33 +0800

    device data

---
+++ src/main/kotlin/app/qur/service/Device.kt
@@ -0,0 +1,3 @@
+package app.qur.service
+
+data class Device(val deviceId: String, var deviceRole: DeviceRole)
---
+++ src/main/kotlin/app/qur/service/DeviceRole.kt
@@ -0,0 +1,7 @@
+package app.qur.service
+
+enum class DeviceRole {
+    SETUP,
+    QUEUE_TAKING_QR_TARGET,
+    QUEUE_DISPLAY,
+}
